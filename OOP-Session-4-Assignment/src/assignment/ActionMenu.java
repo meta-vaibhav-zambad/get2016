4211824d13ab5ceefa3af0ba2d2ea5c04ec52ee7
metacube-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-07-26 15:05:25 +0530

    Files Modified , Comments Added

---
+++ OOP-Session-4-Assignment/src/assignment/ActionMenu.java
@@ -0,0 +1,19 @@
+/**
+ * @author vaibhav zambad
+ * 
+ * Date : 25 July 2016
+ * 
+ * Aim : to apply the design of the menu to replace the main program with menu related classes
+ *
+ */
+
+package assignment;
+
+/**
+ * Interface to set action at each menu
+ */
+public interface ActionMenu {
+	
+	public void action();
+
+}
