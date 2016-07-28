16302ab933662387c0c499c56ab515ecba06dd59
metacube-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-07-28 10:26:03 +0530

    Bug identified in SocialNetwork.java and solved

--- OOP-Session-5-Assignment/src/assignment/SocialNetwork.java
+++ OOP-Session-5-Assignment/src/assignment/SocialNetwork.java
@@ -19,56 +19,64 @@ public class SocialNetwork {
 	 * Entry method of program
 	 */
 	public static void main(String[] args) {
-		
+
 		// creating object of graph to create a new graph
 		Graph graph = new Graph();
-		
+
 		// console operation object for input-output reading
 		ConsoleOperations consoleOperations = new ConsoleOperations();
-		
+
 		// creating a social network object
 		SocialNetwork socialNetwork = new SocialNetwork();
-		
+
 		// list of type nodes is initialized by calling build graph method
 		List <Node> nodeList = socialNetwork.buildGraph(graph);
-		
+
 		// print social network
 		socialNetwork.printSocialNetwork(nodeList);
-		
-		
+
+
 		// char variable to continue the loop
 		char userPermission = '\0';
-		
+
 		do{
-			
+
 			// user input for searching entity in network
 			String userInput = consoleOperations.getString("Enter name to search in network");
-			
+
 			// getting a node by user input
 			Node userNode = graph.getVertex(userInput);
-			
-			// getting entity from node 
-			Entity entity = userNode.getEntity();
-			
-			// printing the entity data
-			System.out.println("Name: "+entity.getName());
-			
-			System.out.println("Contact Number: "+entity.getContactNumber());
-			
-			System.out.println("Email ID: "+entity.getEmailID());
-			
+
+			if(userNode != null){
+
+				// getting entity from node 
+				Entity entity = userNode.getEntity();
+
+				if(entity != null){
+					// printing the entity data
+					System.out.println("Name: "+entity.getName());
+
+					System.out.println("Contact Number: "+entity.getContactNumber());
+
+					System.out.println("Email ID: "+entity.getEmailID());
+				}
+			}
+			else{
+
+				System.out.println("Name does not exist");
+			}
 			// taking user permission
 			userPermission = consoleOperations.getString("Do You Want to Continue ? "+
-					 "Press Y for Yes || press any key to exit").charAt(0);
-			
-			
+					"Press Y for Yes || press any key to exit").charAt(0);
+
+
 		}while(userPermission == 'y' || userPermission == 'Y' );
-		
+
 		System.out.println("Program Ended");
-		
+
 
 	}
-	
+
 	/**
 	 * @param nodeList
 	 * 
@@ -93,7 +101,7 @@ public class SocialNetwork {
 		}
 
 	}
-	
+
 	/**
 	 * 
 	 * @param graph
@@ -102,23 +110,23 @@ public class SocialNetwork {
 	 * this method builds the graph
 	 */
 	public List<Node> buildGraph(Graph graph) {
-		
-	
+
+
 		FileOperations fileOperations = new FileOperations();
-		
+
 		// reading entity data from file
 		List<Entity> entityList = fileOperations
 				.readDataFromFile("Network_Information.txt");
-		
+
 		// reading relations data from file
 		List<String> connectionList = fileOperations
 				.readConnectionsFromFile("Network_Relations.txt");
-		
+
 		// creating a node list 
 		List<Node> nodeList = new ArrayList<Node>();
 
 		Node node = null;
-		
+
 		// creating a node for each entity. Adding node to nodeList. Adding node to graph 
 		for (Entity entity : entityList) {
 
@@ -128,7 +136,7 @@ public class SocialNetwork {
 
 			graph.addVertex(node, true);
 		}
-		
+
 		// creating a connection from given file . Adding connections to graph
 		for (String string : connectionList) {
 
@@ -143,7 +151,7 @@ public class SocialNetwork {
 				graph.addEdge(nodeOne, nodeTwo);
 			}
 		}
-		
+
 		return nodeList;
 
 	}
