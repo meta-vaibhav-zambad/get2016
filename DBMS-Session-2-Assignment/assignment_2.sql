67d6abfc0c2e9a758f107f7c60c5ea183567953c
meta-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-08-12 11:10:33 +0530

    Update assignment_2.sql

--- DBMS-Session-2-Assignment/assignment_2.sql
+++ DBMS-Session-2-Assignment/assignment_2.sql
@@ -10,4 +10,4 @@ IFNULL(return_date,'Book Not Returned') as Return_Date
 FROM book_return b
 LEFT JOIN Members m
 ON b.member_id = m.member_id
-ORDER BY issue_date DESC ,member_name DESC;
+ORDER BY issue_date ,member_name ;
