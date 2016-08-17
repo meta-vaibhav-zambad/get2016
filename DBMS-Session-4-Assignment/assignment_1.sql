abb04ae4dbe16356fbe1c27e19479298ddd16675
metacube-vaibhav-zambad <vaibhav.zambad@metacube.com>
2016-08-17 14:01:21 +0530

    Files Corrected

--- DBMS-Session-4-Assignment/assignment_1.sql
+++ DBMS-Session-4-Assignment/assignment_1.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*Qu 1:  Write a SELECT command to display name of those members
 who belong to the category as to which member
 “Jon Snow” belongs.
@@ -46,20 +45,3 @@ WHERE book_price = (SELECT MAX(book_price) FROM Books );
 of a book.
 */
 SELECT book_price FROM books WHERE book_price = (SELECT DISTINCT(book_price) FROM Books GROUP BY book_price LIMIT 1,1)
-=======
-SELECT member_name FROM Members WHERE member_category = (SELECT member_category FROM Members WHERE member_name = 'Jon Snow');
-
-SELECT b.book_name , t.title_name , m.member_name , book_issue.due_date FROM Books b
-INNER JOIN book_issue bi ON bi.title_id = b.title_id
-INNER JOIN Titles t ON t.title_id = b.title_id
-INNER JOIN Members m ON m.member_id =  bi.member_id
-WHERE bi.book_return  = NULL;
-
-SELECT b.book_name , b.book_price FROM Books b
-WHERE b.book_price = (SELECT MAX(b.book_price) FROM Books b);
-
-SELECT b.book_name , b.book_price FROM Books b
-WHERE b.book_price = (SELECT DISTINCT(b.book_price) FROM Books GROUP BY b.book_price LIMIT 1,1);
-
-
->>>>>>> 075c7f38541368d1905bcf521944787e7c0917ac
